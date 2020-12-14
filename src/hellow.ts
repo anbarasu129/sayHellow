a4fb25852afc0270539b09a98f2e16d9458d5b30
anbu <anbarasu.129@gmail.com>
2020-12-14 12:48:35 +0530

    changed version and test code

--- src/hellow.ts
+++ src/hellow.ts
@@ -3,7 +3,7 @@ import * as Sleep from "sleep";
 export class Hello {
   public sayHelloTo(name: string): string {
     console.log(`test DateTime1: ${new Date()}`);
-    Sleep.msleep(10);
+    Sleep.sleep(100);
     console.log(`test DateTime2: ${new Date()}`);
     return `🦄 Hello ${name} !`;
   }
