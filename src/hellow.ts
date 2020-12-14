7d2ae30a3936fec003bfdfdc6d8084ea39cb014f
anbu <anbarasu.129@gmail.com>
2020-12-14 12:39:25 +0530

    added sleep package for testing

--- src/hellow.ts
+++ src/hellow.ts
@@ -1,5 +1,10 @@
+import * as Sleep from "sleep";
+
 export class Hello {
-    public sayHelloTo(name: string): string {
-      return `🦄 Hello ${name} !`;
-    }
+  public sayHelloTo(name: string): string {
+    console.log(`test DateTime1: ${new Date()}`);
+    Sleep.msleep(10);
+    console.log(`test DateTime2: ${new Date()}`);
+    return `🦄 Hello ${name} !`;
   }
+}
